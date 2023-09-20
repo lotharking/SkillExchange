d066c4d949042e2dd86b4a7e10fea4cf88da60be
Andres Vallecilla <andresfelipe083195@hotmail.com>
2023-09-20 12:33:06 -0500

    Feat: create authentication service

---
+++ BackEnd/AppAuthentication/src/main/java/com/skill/exchange/AppAuthentication/AppAuthenticationApplication.java
@@ -0,0 +1,13 @@
+package com.skill.exchange.AppAuthentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppAuthenticationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppAuthenticationApplication.class, args);
+	}
+
+}
---
+++ BackEnd/AppAuthentication/src/test/java/com/skill/exchange/AppAuthentication/AppAuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skill.exchange.AppAuthentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppAuthenticationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
