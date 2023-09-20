a6ab4cc1b4912df59abcdf33f2ddbac4abd827c9
Andres Vallecilla <andresfelipe083195@hotmail.com>
2023-09-20 11:45:51 -0500

    Feat: create project

---
+++ BackEnd/AppApiGateway/src/main/java/com/skill/exchange/AppApiGateway/AppApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.skill.exchange.AppApiGateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppApiGatewayApplication.class, args);
+	}
+
+}
---
+++ BackEnd/AppApiGateway/src/test/java/com/skill/exchange/AppApiGateway/AppApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skill.exchange.AppApiGateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
