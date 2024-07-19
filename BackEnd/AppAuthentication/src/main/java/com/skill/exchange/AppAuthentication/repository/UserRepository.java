6dffa432d20ab3118ad6615b22e6aeb57c552783
Andres Vallecilla <andresfelipe083195@hotmail.com>
2024-07-19 01:38:29 -0500

    feat: implement docker compose file with eureka server

--- BackEnd/AppAuthentication/src/main/java/com/skill/exchange/AppAuthentication/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.skill.exchange.AppAuthentication.repository;
-
-import com.skill.exchange.AppAuthentication.model.User;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface UserRepository extends MongoRepository<User, String> {
-    
-}
